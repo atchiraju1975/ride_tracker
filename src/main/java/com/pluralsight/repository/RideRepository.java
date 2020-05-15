fb9f9f0a3d62fc2361173cdfcc4607a01e120097
SrinivasaChakka <Srinivasa.Chakka@tesco.com>
2020-05-16 05:23:07 +0530

    fixed compatibility issues and all errors
    there were errors and project was not working due to difference in java compiler version and mysql version

---
+++ src/main/java/com/pluralsight/repository/RideRepository.java
@@ -0,0 +1,12 @@
+package com.pluralsight.repository;
+
+import java.util.List;
+
+import com.pluralsight.model.Ride;
+
+public interface RideRepository {
+
+	Ride createRide(Ride ride);
+	List<Ride> getRides();
+
+}
